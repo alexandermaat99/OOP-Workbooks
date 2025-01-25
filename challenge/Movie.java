0807660c7e7e3357c1384f2f5481cbd2c5e66f6b
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-01-25 11:11:23 -0700

    init workbooks

---
+++ challenge/Movie.java
@@ -0,0 +1,46 @@
+public class Movie {
+
+  //attributes
+  private String name;
+  private String format;
+  private double rating;
+
+  //constructor
+  public Movie(String name, String format, double rating) {
+    this.name = name;
+    this.format = format;
+    this.rating = rating;
+  }
+
+  //copy constructor
+  public Movie(Movie source) {
+    this.name = source.getName();
+    this.format = source.getFormat();
+    this.rating = source.getRating();
+  }
+
+  //getters and setters
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+
+  public String getFormat() {
+    return format;
+  }
+
+  public void setFormat(String newFormat) {
+    this.format = newFormat;
+  }
+
+  public double getRating() {
+    return rating;
+  }
+
+  public void setRating(double newRating) {
+    this.rating = newRating;
+  }
+}
