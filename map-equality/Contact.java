eecea79ee8d05f98e962bf5bd65c02df85ab99dd
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-14 11:34:56 -0700

    done with hashmap equality

--- map-equality/Contact.java
+++ map-equality/Contact.java
@@ -1,34 +1,36 @@
 public class Contact {
 
-    private String name;
-    private int age;
+  private String name;
+  private int age;
 
-    public Contact(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+  public Contact(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    public Contact(Contact source) {
-        this.name = source.name;
-        this.age = source.age;
-    }
+  public Contact(Contact source) {
+    this.name = source.name;
+    this.age = source.age;
+  }
 
+  public String getName() {
+    return this.name;
+  }
 
-    public String getName() {
-        return this.name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public int getAge() {
+    return this.age;
+  }
 
-    public int getAge() {
-        return this.age;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-
+  @Override
+  public boolean equals(Object o){
+    if 
+  }
 }
