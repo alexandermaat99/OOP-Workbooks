6d63e5de512c4a9c76c6ddf0edf09ccde0166bef
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-15 11:58:49 -0700

    getting better

--- capitalProblem.java
+++ capitalProblem.java
@@ -3,12 +3,21 @@
 public class capitalProblem {
 
   public static void main(String[] args) {
-    System.out.println("testing");
-    // System.out.println(isCapital("Fred") == true);
-    // System.out.println(isCapital("fred") == false);
-    // System.out.println(isCapital("") == false);
-    // System.out.println(isCapital("2") == false);
-    // System.out.println(isCapital("a") == false);
-    // System.out.println(isCapital(null) == false);
+    System.out.println(isCapital("Fred") == true);
+    System.out.println(isCapital("fred") == false);
+    System.out.println(isCapital("") == false);
+    System.out.println(isCapital("2") == false);
+    System.out.println(isCapital("a") == false);
+    System.out.println(isCapital(null) == false);
+  }
+
+  public static boolean isCapital(String wordToCheck) {
+    if (wordToCheck == null || wordToCheck.isBlank()) {
+      return false;
+    }
+
+    char firstLetter = wordToCheck.charAt(0);
+
+    return ((firstLetter >= 'A') & (firstLetter <= 'Z')) ? true : false;
   }
 }
