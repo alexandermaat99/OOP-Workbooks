e658d57e956a0ac339e8e9b404279ede47e0250a
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-14 23:49:19 -0700

    part 1 done

---
+++ capitalProblem.java
@@ -0,0 +1,14 @@
+//Write a function that returns true if the first letter of a string is Capitalized
+
+public class capitalProblem {
+
+  public static void main(String[] args) {
+    System.out.println("testing");
+    // System.out.println(isCapital("Fred") == true);
+    // System.out.println(isCapital("fred") == false);
+    // System.out.println(isCapital("") == false);
+    // System.out.println(isCapital("2") == false);
+    // System.out.println(isCapital("a") == false);
+    // System.out.println(isCapital(null) == false);
+  }
+}
--- starter/constants/Position.java
+++
@@ -1,9 +0,0 @@
-package constants;
-
-public enum Position {
-  SHOOTING_GUARD,
-  SMALL_FORWARD,
-  POWER_FORWARD,
-  CENTER,
-  POINT_GUARD,
-}
