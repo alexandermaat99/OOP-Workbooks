6b835a0b3f5248f78117c416fe1a34117fc4df12
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-19 10:23:04 -0700

    init capstone

--- starter-project/Main.java
+++ starter-project/Main.java
@@ -1,43 +1,5 @@
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import pojo.Account;
-import pojo.Checking;
-import pojo.Credit;
-import repository.AccountRepository;
-import service.AccountService;
-import service.CheckingService;
-import service.CreditService;
-
 public class Main {
-
-  public static void main(String[] args) {
-    AccountRepository repository = new AccountRepository();
-
-
-    AccountService accountService = new CheckingService(repository);
-    accountService.deposit("1", new BigDecimal("100"));
-    accountService.withdraw("1", new BigDecimal("50"));
-
-
-
-    CreditService creditService = new CreditService(repository);
-
-    // Assume these were obtained from user input.
-    List<Account> accounts = Arrays.asList(
-      new Checking("A1234B", new BigDecimal("500.00")),
-      new Checking("E3456F", new BigDecimal("300.50")),
-      new Checking("I5678J", new BigDecimal("100.25")),
-      new Credit("A1534B", new BigDecimal("0.50")),
-      new Credit("G4567H", new BigDecimal("200.00"))
-    );
-
-    accounts.forEach(account -> {
-      if (account instanceof Checking) {
-        checkingService.createAccount((Checking) account);
-      } else {
-        creditService.createAccount((Credit) account);
-      }
-    });
-  }
+    public static void main(String[] args) {
+        
+    }
 }
