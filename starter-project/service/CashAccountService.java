dca5684ca5478ea0801b95770bc70c8646c1e99c
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-19 12:56:49 -0700

    part 3 done

--- starter-project/service/CashAccountService.java
+++ starter-project/service/CashAccountService.java
@@ -1,7 +1,52 @@
 package service;
 
+import java.math.BigDecimal;
+import pojo.CashAccount;
+import repository.*;
 
-public class CashAccountService {
+public class CashAccountService implements TradeAccountService {
 
+  private TradeAccountRepository repository;
 
+  //this constructor is what links this service layer to the repo. This class is called from the main(presentation layer) so it
+  //essentially links the presentation layer to the repo
+
+  public CashAccountService(TradeAccountRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public void deposit(String id, BigDecimal amount) {
+    CashAccount account = retrieveTradeAccount(id);
+    account.setCashBalance(account.getCashBalance().add(amount));
+    updateTradeAccount(account);
+  }
+
+  @Override
+  public void withdraw(String id, BigDecimal amount) {
+    //CashAccount is a type, retrieving an existing account i name account, I'm retrieving using the id parameter, it clones it in the repo layer
+    CashAccount account = retrieveTradeAccount(id);
+
+    //setting the cash balance as the cash balance pulled in, subtracted by the amount parameter
+    account.setCashBalance(account.getCashBalance().subtract(amount));
+
+    //the account is then updated
+    updateTradeAccount(account);
+  }
+
+  public void createTradeAccount(CashAccount account) {
+    this.repository.createTradeAccount(account);
+  }
+
+  public CashAccount retrieveTradeAccount(String id) {
+    return (CashAccount) this.repository.retrieveTradeAccount(id);
+  }
+
+  public void updateTradeAccount(CashAccount account) {
+    this.repository.updateTradeAccount(account);
+  }
+
+  public void deleteTradeAccount(String id) {
+    this.repository.deleteTradeAccount(id);
+  }
 }
--- starter-project/service/MarginAccountService.java
+++ starter-project/service/MarginAccountService.java
@@ -1,7 +1,51 @@
 package service;
 
+import java.math.BigDecimal;
+import pojo.*;
+import repository.TradeAccountRepository;
 
-public class MarginAccountService {
+public class MarginAccountService implements TradeAccountService {
 
+  TradeAccountRepository repository;
 
+  //constructor
+  public MarginAccountService(TradeAccountRepository repository) {
+    this.repository = repository;
+  }
+
+  //withdraw - interface
+  @Override
+  public void withdraw(String id, BigDecimal amount) {
+    MarginAccount account = retrieveTradeAccount(id);
+    account.setMargin(account.getMargin().subtract(amount));
+    updateTradeAccount(account);
+  }
+
+  //deposit - interface
+  @Override
+  public void deposit(String id, BigDecimal amount) {
+    MarginAccount account = retrieveTradeAccount(id);
+    account.setMargin(account.getMargin().add(amount));
+    updateTradeAccount(account);
+  }
+
+  //createTradeAccount
+  public void createTradeAccount(MarginAccount account) {
+    this.repository.createTradeAccount(account);
+  }
+
+  //retrieveTradeAccount
+  public MarginAccount retrieveTradeAccount(String id) {
+    return (MarginAccount) this.repository.retrieveTradeAccount(id);
+  }
+
+  //updateTradeAccount
+  public void updateTradeAccount(MarginAccount account) {
+    this.repository.updateTradeAccount(account);
+  }
+
+  //deleteTradeAccount
+  public void deleteTradeAccount(String id) {
+    this.repository.deleteTradeAccount(id);
+  }
 }
